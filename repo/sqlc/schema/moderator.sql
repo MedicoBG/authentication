1ca1cfbb505f35c34a3d6ed48116202328050c71
IvayloKraev <ivaylokraev@protonmail.com>
2025-03-15 19:23:19 +0200

    Create SQL schema for admin and moderator auth

---
+++ repo/sqlc/schema/moderator.sql
@@ -0,0 +1,15 @@
+CREATE TYPE moderator_type AS ENUM (
+    'doctor',
+    'pharmacy',
+    'citizen',
+    'medicament'
+    );
+
+
+CREATE TABLE moderator_auth
+(
+    id       uuid  NOT NULL DEFAULT gen_random_uuid(),
+    email    text  NOT NULL,
+    password bytea NOT NULL,
+    type moderator_type NOT NULL
+);
